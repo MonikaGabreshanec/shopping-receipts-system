0212b8916b1ec5fe2cd3ac38c47ab75f6cdb1f44
MonikaGabreshanec <monikagabreshanec@gmail.com>
2025-05-13 20:11:31 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/uiktp/shoppingreceiptssystem/ShoppingReceiptsSystemApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.uiktp.shoppingreceiptssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingReceiptsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingReceiptsSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/uiktp/shoppingreceiptssystem/ShoppingReceiptsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.uiktp.shoppingreceiptssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingReceiptsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
